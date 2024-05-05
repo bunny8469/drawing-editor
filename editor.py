e85f1023dfbd86538e4979facfeb3b5d37c1335c
bunny8469 <saipraneethbommana@gmail.com>
2024-05-06 00:23:52 +0530

    Select bounding box

--- editor.py
+++ editor.py
@@ -40,6 +40,14 @@ def random_color():
     color_hex = "#{:02x}{:02x}{:02x}".format(red, green, blue)
     return color_hex
 
+# class DrawingObject:
+#     def __init__(self):
+#         pass
+
+# class Line:
+#     def __init__(self):
+
+
 class DrawingEditor:
     def __init__(self, master):
         self.master = master
@@ -57,12 +65,14 @@ class DrawingEditor:
         self.line_button = Button(self.left_panel, title="Draw Line", command=lambda: self.set_current_object("line"), **button_style)
         self.line_button.config(width=20)
         self.line_button.pack(side=tk.TOP, fill=tk.X)
-        # self.line_button.bind("<Button-1>", self.set_current_object("line"))
 
         self.rect_button = Button(self.left_panel, title="Draw Rectangle", command=lambda: self.set_current_object("rectangle"), **button_style)
         self.rect_button.config(width=20)
         self.rect_button.pack(fill=tk.X)
-        # self.line_button.bind("<Button-1>", self.set_current_object("rectangle"))
+        
+        self.rect_button = Button(self.left_panel, title="Select", command=lambda: self.set_current_object(None), **button_style)
+        self.rect_button.config(width=20)
+        self.rect_button.pack(fill=tk.X)
 
         self.selected_object = None
         self.objects = []
@@ -72,17 +82,17 @@ class DrawingEditor:
 
         # Event bindings
         self.canvas.bind("<Button-1>", self.on_canvas_click)
-        self.canvas.bind("<ButtonRelease-1>", self.on_canvas_release)
         self.canvas.bind("<B1-Motion>", self.on_mouse_drag)
+        self.canvas.bind("<ButtonRelease-1>", self.on_canvas_release)
 
-        self.drawing = False
         self.start_x = None
         self.start_y = None
+        self.drawing = False
         self.current_object = None
+        self.select = False
 
     def set_current_object(self, object_type):
         self.selected_object = object_type
-        print(self.selected_object)
 
     def create_toolbar(self):
         # Create toolbar buttons or menu here
@@ -94,28 +104,35 @@ class DrawingEditor:
         self.start_y = event.y
         self.drawing = True
 
+        if self.current_object and not self.selected_object:
+            self.canvas.delete(self.current_object)
+
     def on_canvas_release(self, event):
         # Handle canvas release event
         if self.drawing:
-            self.draw_object(self.selected_object, self.start_x, self.start_y, event.x, event.y)
-            self.drawing = False
             if self.current_object:
                 self.canvas.delete(self.current_object)  # Delete the temporary line
+                self.current_object = self.draw_object(self.selected_object, self.start_x, self.start_y, event.x, event.y)
+                self.select = self.selected_object
+            self.drawing = False
 
     def on_mouse_drag(self, event):
         # Handle mouse drag event
         if self.drawing:
-            if self.current_object:
+            if self.current_object and not self.select:
                 self.canvas.delete(self.current_object)  # Delete previous temporary line
-            
             self.current_object = self.draw_object(self.selected_object, self.start_x, self.start_y, event.x, event.y, fill="black")
+            self.select = True
+            
 
     def draw_object(self, object_type, start_x, start_y, end_x, end_y, **kwargs):
         # Draw object on canvas based on type and coordinates
         if object_type == "line":
             return self.canvas.create_line(start_x, start_y, end_x, end_y, fill=random_color())
         elif object_type == "rectangle":
-            return self.canvas.create_rectangle(start_x, start_y, end_x, end_y, outline=random_color())
+            return self.canvas.create_rectangle(start_x, start_y, end_x, end_y, fill=random_color())
+        else:
+            return self.canvas.create_rectangle(start_x, start_y, end_x, end_y, outline=random_color(), dash=(4, 2))
 
     def select_object(self, object):
         # Select object on canvas
